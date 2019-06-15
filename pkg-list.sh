65cdab7ec07b7784caa57d9fcda437c77fe9f593
Brent Lintner <brent.lintner@gmail.com>
2019-06-15 00:08:46 -0400

    Should re-enable pkg list tracking on arch.

--- pkg-list.sh
+++ pkg-list.sh
@@ -2,10 +2,8 @@
 
 BACKUP_FILE=/home/brent/.pacman-installed.txt
 
-trizen --stats > $BACKUP_FILE
-
 echo -e "\n\nAUR installed:\n" >> $BACKUP_FILE
-trizen -Qm >> $BACKUP_FILE
+yay -Qm >> $BACKUP_FILE
 
 echo -e "\n\nSystem installed:\n" >> $BACKUP_FILE
-trizen -Qn >> $BACKUP_FILE
+yay -Qn >> $BACKUP_FILE
