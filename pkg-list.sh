a132b132c1edb07a1eaa63d26f87b4ac925283f8
Brent Lintner <brent.lintner@gmail.com>
2019-09-03 14:32:21 -0400

    Fix pkg list.

--- pkg-list.sh
+++ pkg-list.sh
@@ -3,7 +3,7 @@
 BACKUP_FILE=/home/brent/.pacman-installed.txt
 
 echo -e "\n\nAUR installed:\n" >> $BACKUP_FILE
-yay -Qm >> $BACKUP_FILE
+yay -Qm > $BACKUP_FILE
 
 echo -e "\n\nSystem installed:\n" >> $BACKUP_FILE
 yay -Qn >> $BACKUP_FILE
