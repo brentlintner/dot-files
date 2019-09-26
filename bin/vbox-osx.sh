2855296b995ede3ccaf6e54343fca3090d61d8c6
Brent Lintner <brent.lintner@gmail.com>
2019-09-26 11:37:10 -0400

    More tweaks to vm script.

--- bin/vbox-osx.sh
+++ bin/vbox-osx.sh
@@ -5,9 +5,13 @@ set -xe
 name=$1
 
 VBoxManage modifyvm "$name" --cpuidset 00000001 000106e5 00100800 0098e3fd bfebfbff
-VBoxManage setextradata "$name" "VBoxInternal/Devices/efi/0/Config/DmiSystemProduct" "iMac11,3"
+VBoxManage modifyvm "$name" --cpu-profile "Intel Core i7-6700K"
+#VBoxManage setextradata "$name" "VBoxInternal/Devices/efi/0/Config/DmiSystemProduct" "iMac11,3"
+VBoxManage setextradata "$name" "VBoxInternal/Devices/efi/0/Config/DmiSystemProduct" "MacPro6,1"
 VBoxManage setextradata "$name" "VBoxInternal/Devices/efi/0/Config/DmiSystemVersion" "1.0"
 VBoxManage setextradata "$name" "VBoxInternal/Devices/efi/0/Config/DmiBoardProduct" "Iloveapple"
 VBoxManage setextradata "$name" "VBoxInternal/Devices/smc/0/Config/DeviceKey" "ourhardworkbythesewordsguardedpleasedontsteal(c)AppleComputerInc"
 VBoxManage setextradata "$name" "VBoxInternal/Devices/smc/0/Config/GetKeyFromRealSMC" 1
-VBoxManage modifyvm "$name" --cpu-profile "Intel Core i7-6700K"
+VBoxManage setextradata "$name" VBoxInternal2/EfiGopMode 5
+VBoxManage setextradata "$name" VBoxInternal2/EfiGraphicsResolution 1920x1080
+VBoxManage setextradata "$name" "VBoxInternal/Devices/efi/0/LUN#0/Config/PermanentSave" 1
