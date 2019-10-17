8cb0b40af620f65a103a8a4eaeca2fda74e1ff9d
Brent Lintner <brent.lintner@gmail.com>
2019-10-17 16:46:59 -0400

    Adding a bunch of small service/scripts for my arch install.

---
+++ bin/enable-nvidia-pm.sh
@@ -0,0 +1,2 @@
+#!/usr/bin/env sh
+echo 'auto' > '/sys/bus/i2c/devices/i2c-11/device/power/control'
--- bin/pkg-list.sh
+++ bin/pkg-list.sh
@@ -3,7 +3,7 @@
 BACKUP_FILE=/home/brent/.pacman-installed.txt
 
 echo -e "\n\nAUR installed:\n" >> $BACKUP_FILE
-yay -Qm > $BACKUP_FILE
+sudo -u brent yay -Qm > $BACKUP_FILE
 
 echo -e "\n\nSystem installed:\n" >> $BACKUP_FILE
-yay -Qn >> $BACKUP_FILE
+sudo -u brent yay -Qn >> $BACKUP_FILE
