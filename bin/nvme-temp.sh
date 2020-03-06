f71561c2c2e5d7697965221e8d183710b4f8127a
Brent Lintner <brent.lintner@gmail.com>
2020-03-05 20:47:03 -0500

    Fix script for nvme0 temp.

--- bin/nvme-temp.sh
+++ bin/nvme-temp.sh
@@ -5,7 +5,7 @@ tempfile="/tmp/nvme0-temp"
 trap "rm -f "$tempfile"; exit" 0 1 2 3 13 15  # Exit, HUP, INT, QUIT, PIPE, TERM
 
 while true; do
-    temperature="$(nvme smart-log /dev/nvme0 | /usr/bin/sed -n '/^[Tt]emperature *: *\([0-9]*\).*/{s//\1/p;q}')"
+    temperature="$(nvme smart-log /dev/nvme0 | /usr/bin/grep -i temperature | head -n 1 | cut -c 18-19)"
     echo "$temperature" > "$tempfile"
     sleep 2
 done
