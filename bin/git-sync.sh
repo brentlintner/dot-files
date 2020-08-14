75ee6d48592a3a6b83aea13e7aec25cf688b0d41
Brent Lintner <brent.lintner@gmail.com>
2020-08-14 16:51:25 -0400

    The git-sync command should stash as well just in case.

--- bin/git-sync.sh
+++ bin/git-sync.sh
@@ -3,6 +3,7 @@
 CURRENT_BRANCH=`git br --show-current`
 MAIN_BRANCH=$1
 
+git stash
 git checkout $MAIN_BRANCH
 git fetch --all --prune
 git merge
@@ -14,3 +15,5 @@ BRANCHES=`git branch --merged | grep -v "\*" | grep -v $MAIN_BRANCH`
 if [ ! -z "$BRANCHES" ]; then
   git branch -d $BRANCHES
 fi
+
+git stash pop
